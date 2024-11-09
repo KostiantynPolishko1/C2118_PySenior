b1c994a9e83cc1cc05f058e1327b29188953f40f
polishko_k <kostantinpolishko@gmail.com>
2024-11-09 10:02:31 +0200

    1 inspect class Student

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import inspect
+from lesson4 import Student
+
 print('Lesson 5: Class Inspect')
+
+print('is function:', inspect.isfunction(Student))
+print('is class: ', inspect.isclass(Student))
+
+sigStudent = inspect.signature(Student)
+
+print(sigStudent.parameters)
+for item in sigStudent.parameters.items():
+    print(f'item - {item[1]}')
