896e76a98d928906dc1cd58815bd45fc9793ff44
polishko_k <kostantinpolishko@gmail.com>
2024-11-09 11:29:35 +0200

    1 class ItemSoft with handle try-except

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
-import inspect
+# import inspect
+#
+# from lesson4 import Student
+#
+print('Lesson 6: Try-Except')
+#
+# print('is function:', inspect.isfunction(Student))
+# print('is class: ', inspect.isclass(Student))
+#
+# #contruction try-except-else-finally
+#
+# try:
+#     print('is method', inspect.ismethod(Student.age))
+# except Exception as exception:
+#     print(f'error: {exception.__str__()}')
+# finally:
+#     print('Checking is done')
+#
+# sigStudent = inspect.signature(Student)
+#
+# print(sigStudent.parameters)
+# for item in sigStudent.parameters.items():
+#     print(f'item - {item[1]}')
 
-from lesson4 import Student
 
-print('Lesson 5: Class Inspect')
 
-print('is function:', inspect.isfunction(Student))
-print('is class: ', inspect.isclass(Student))
+class ItemSoft:
+    price: int
+    mark: str
 
-sigStudent = inspect.signature(Student)
+    def __init__(self, price: int, mark: str):
+        if type(price) is not int:
+            raise TypeError('argument price is not int')
+        self.price = price
+        self.mark = mark
 
-print(sigStudent.parameters)
-for item in sigStudent.parameters.items():
-    print(f'item - {item[1]}')
+
+try:
+    keyboard = ItemSoft('1000a', 'logitech')
+    print(f'name : {keyboard.mark}, price : {keyboard.price}/{type(keyboard.price)}')
+except Exception as error:
+    print(f'error msg: {error.__str__()}')
+finally:
+    print('init of instance is done')
