0d394779dfc00f9260241e07137f512492f51e35
polishko_k <kostantinpolishko@gmail.com>
2024-11-16 11:18:43 +0200

    1 create exemaple of logs

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Lesson : ')
+print('Lesson 8: Log and UnitTest')
+
+
+import logging
+
+
+logging.basicConfig(level=logging.WARNING, filename='pysenior.log', filemode='w',
+                    format='time%(asctime)s:level%(levelname)s:description-%(message)s')
+
+# logging.debug('log level debug')
+# logging.info('log level info')
+# logging.warning('log level warning')
+# logging.error('log level error')
+# logging.critical('log level critical')
+
+logging.info('launch app')
+
+numbers = [2, 5, 3, 1, 0, 7, 9, 8]
+
+try:
+    logging.warning('treat list by cycle')
+    for i in range(0, 10):
+        print(f'index {i}: element{numbers[i]}')
+
+except Exception as error:
+    logging.error(error.__str__())
+    print(error.__str__())
