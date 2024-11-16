8b4859f71d59897f789fad74c08a6622c40a9d02
polishko_k <kostantinpolishko@gmail.com>
2024-11-16 09:44:45 +0200

    1 created class Counter

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 print('Lesson 7: Iterators, Closure function')
+
+
+class Counter:
+    limit: int
+    index: int
+
+    def __init__(self, limit: int):
+        self.index = 0
+        self.limit = limit
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        self.index += 3
+        if self.index > self.limit:
+            raise StopIteration
+        return self.index
+
+
+numbers = ['one', 'two', 3, 4, 'five', 'c++', 'c#']
+counter = Counter(10)
+for i in counter:
+    print(numbers[i])
