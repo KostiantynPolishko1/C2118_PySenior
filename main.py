0c2ed2c22ef08b0e118309f5a61286d303afc7ec
polishko_k <kostantinpolishko@gmail.com>
2024-11-23 10:48:55 +0200

    3 done logic for get value rate object coin

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ print('Lesson 9: Parsing Site')
 
 responce = requests.get('https://coinmarketcap.com/')
 
+
 if responce.status_code == 200:
 
+    # print('before parser', responce.text)
     item_site = BeautifulSoup(responce.text, features='html.parser')
-    coins = item_site.find_all('div', {'class', 'sc-b3fc6b7-0 dzgUIj'})
+    coin_rates = item_site.find_all('div', {'class', 'sc-b3fc6b7-0 dzgUIj'})
 
-    for coin in coins:
-        print('coin -> ', coin)
+    for rate in coin_rates:
+        # print('rate object -> ', rate)
+        price = float(rate.findNext().text[1:].replace(',', ''))
+        print('price -> ', price)
+
