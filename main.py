ac854276f8100eb1f2caf45484cc77f54f3bb3fc
polishko_k <kostantinpolishko@gmail.com>
2024-11-23 10:06:49 +0200

    1 done logic for get object coin

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
 print('Lesson 9: Parsing Site')
+
+responce = requests.get('https://coinmarketcap.com/')
+
+if responce.status_code == 200:
+
+    item_site = BeautifulSoup(responce.text, features='html.parser')
+    coins = item_site.find_all('div', {'class', 'sc-b3fc6b7-0 dzgUIj'})
+
+    for coin in coins:
+        print('coin -> ', coin)
